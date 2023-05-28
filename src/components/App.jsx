23fe135193e5fb2f72e408da2f1c67e33212dddf
Iryna Rozmaita <iriszechka@ukr.net>
2023-05-28 15:13:56 +0300

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Profile } from './Profile/Profile';
+import user from 'data/user';
+import data from 'data/data';
+import { Statistics } from './Statistics/Statistics';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+   <div>
+     <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
+    
   );
 };
+
