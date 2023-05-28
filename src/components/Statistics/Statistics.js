db528cbd9e2f01c4975e166bb6452d30a79105a9
Iryna Rozmaita <iriszechka@ukr.net>
2023-05-28 15:23:55 +0300

    +Statistics

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types'; 
+import css from './Statistics.module.css';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+export const Statistics = ({ title, stats }) => {
+    return (
+     <section className={css.statistics}>
+     {title && <h2 className={css.title}>{title}</h2>}
+            
+    <ul className={css.stat__list}>
+    {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.stats}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>         
+</section>
+)
+}
+
+Statistics.propTypes = {
+title: PropTypes.string,
+stats: PropTypes.array.isRequired,
+label: PropTypes.string.isRequired,
+percentage: PropTypes.number.isRequired,    
+}
