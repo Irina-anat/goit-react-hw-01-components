b0e1e1fd06c547da2484156fa672b5645d21c2c6
Iryna Rozmaita <iriszechka@ukr.net>
2023-05-28 15:19:15 +0300

    + data.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+       <Statistics title="Upload stats" stats={data}/>
     </div>
     
   );
